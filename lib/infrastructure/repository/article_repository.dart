2e407e74b1c7622873c75dfffab3cc4baa78ffbb
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-05 09:26:53 +0900

    Add: repository実装

---
+++ lib/infrastructure/repository/article_repository.dart
@@ -0,0 +1,10 @@
+import 'package:kadai_info_flutter/domain/entity/article_list.dart';
+import 'package:kadai_info_flutter/domain/repository/article/i_article_repository.dart';
+
+class ArticleRepository implements IArticleRepository {
+  @override
+  Future<ArticleList> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+}
