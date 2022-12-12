0fa2cf4a9f56a7820c97282f2d727d824486522c
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-12 09:34:53 +0900

    Update wordpress_api_datasource_provider.dart

--- lib/infrastructure/datasource/wordpress_api/wordpress_api_datasource_provider.dart
+++ lib/infrastructure/datasource/wordpress_api/wordpress_api_datasource_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kadai_info_flutter/infrastructure/datasource/wordpress_api/i_wordpress_api_datasource.dart';
 import 'package:kadai_info_flutter/infrastructure/datasource/wordpress_api/wordpress_api_datasource.dart';
@@ -12,7 +13,7 @@ final wordpressApiDatasourceProvider = Provider<IWordPressApiDatasource>(
         baseUrl: 'https://kadai-info.com/wp-json/wp/v2',
         headers: {
           'Authorization':
-              'Basic ${base64Encode('kadaiinfo:n4VM mTic XdBm osHt Vddv rV81'.codeUnits)}', // FIXME: 環境変数等にいれる
+              'Basic ${base64Encode('${dotenv.env['WORDPRESS_USER_ID']}:${dotenv.env['WORDPRESS_USER_PASS']}'.codeUnits)}',
         },
       ),
     ),
